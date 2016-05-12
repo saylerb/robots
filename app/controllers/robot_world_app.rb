87f614418a68050b6caf92a6418700c8dda65066
saylerb <sayler.b@gmail.com>
2016-05-11 22:42:38 -0600

    add ability to write new robot to YAML database

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,3 +1,6 @@
+require "yaml/store"
+require "models/robot_manager"
+
 class RobotWorldApp < Sinatra::Base
   set :root, File.expand_path("..", __dir__)
 
@@ -13,5 +16,17 @@ class RobotWorldApp < Sinatra::Base
   get "/robots/new" do
     haml :new
   end
+
+  post "/robots" do
+    robot_manager.create(params[:robot])
+    redirect "/tasks"
+  end
+
+  def robot_manager
+    database = YAML::Store.new("db/robot_manager")
+    @robot_manager ||= RobotManager.new(database)
+  end
+
+
 
 end
---
+++ app/models/robot_manager.rb
@@ -0,0 +1,25 @@
+class RobotManager
+
+  attr_reader :database
+
+  def initialize(database)
+    @database = database
+  end
+
+  def create(robot)
+    database.transaction do 
+      database["robots"] ||= []
+      database["total"] ||= 0
+      database["total"] += 1
+      database["robots"] << { "id" => database["total"],
+                              "name" => robot[:name], 
+                              "state" => robot[:state], 
+                              "avatar" => robot[:avatar], 
+                              "birthdate" => robot[:birthdate],
+                              "date_hired" => robot[:date_hired],
+                              "department" => robot[:department] }
+
+    end
+  end
+
+end
